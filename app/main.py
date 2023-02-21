e281ee89e8306d9bd0f91832b214893de02601fd
Cosmic-rare <ysolakey1116@gmail.com>
2023-02-22 07:14:09 +0900

    Clean

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, Depends, File, HTTPException, UploadFile, status
+from fastapi import FastAPI
 from score import score
 from title import title
 from difficult import difficult
@@ -12,7 +12,7 @@ def upload_file(url):
     difficultData = difficult(url, titleData["music"]["id"])
 
     return {
-            "score": scoreData, 
-            "title": titleData, 
-            "difficult": difficultData
-        }
+             "score": scoreData, 
+             "title": titleData, 
+             "difficult": difficultData
+           }
