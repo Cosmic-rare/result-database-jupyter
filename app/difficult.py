c307245957375581791f0e0beaf0cb26dc40a298
Cosmic-rare <ysolakey1116@gmail.com>
2023-02-23 10:53:47 +0900

    format by black

--- app/difficult.py
+++ app/difficult.py
@@ -14,22 +14,24 @@ if path_tesseract not in os.environ["PATH"].split(os.pathsep):
 tools = pyocr.get_available_tools()
 tool = tools[0]
 
+
 def getDifficult(musicId, difficult):
-    with open('./json/difficult.json', encoding="utf-8") as f1:
+    with open("./json/difficult.json", encoding="utf-8") as f1:
         difficulties = json.load(f1)
 
         data = "n/a"
 
         for i in difficulties:
             if i["musicId"] == musicId and difficult.lower() == i["musicDifficulty"]:
-                data = (i)
+                data = i
 
         return data
 
-def check(target, musicId):
-    difficults = ["EASY","NORMAL","HARD","EXPERT","MASTER"]
 
-    data = {"credibility":0,"musicDifficulty":""}
+def check(target, musicId):
+    difficults = ["EASY", "NORMAL", "HARD", "EXPERT", "MASTER"]
+
+    data = {"credibility": 0, "musicDifficulty": ""}
 
     for j in difficults:
         result = difflib.SequenceMatcher(None, target, j).ratio()
@@ -44,40 +46,43 @@ def check(target, musicId):
 
     return data
 
+
 def difficult(url, musicId):
     img = openImg(url)
-    rgb_img = img.convert('RGB')
+    rgb_img = img.convert("RGB")
     size = rgb_img.size
 
-    crop_img = rgb_img.crop([0, 0, size[0]/2, size[1]/7])
+    crop_img = rgb_img.crop([0, 0, size[0] / 2, size[1] / 7])
     crop_size = crop_img.size
 
-    resize_img = crop_img.resize((math.floor(crop_size[0]/15), math.floor(crop_size[1]/15)))
-    
-    img2 = Image.new('RGBA',crop_size)
+    resize_img = crop_img.resize(
+        (math.floor(crop_size[0] / 15), math.floor(crop_size[1] / 15))
+    )
+
+    img2 = Image.new("RGBA", crop_size)
 
     for x in range(crop_size[0]):
         for y in range(crop_size[1]):
-            r,g,b = crop_img.getpixel((x,y))
+            r, g, b = crop_img.getpixel((x, y))
 
-            if 94<r<175 and 179<g<255 and 28<b<108:
+            if 94 < r < 175 and 179 < g < 255 and 28 < b < 108:
                 a = 0
-            elif 54<r<134 and 144<g<224 and 192<b<255:
+            elif 54 < r < 134 and 144 < g < 224 and 192 < b < 255:
                 a = 0
-            elif 204<r<255 and 135<g<215 and 21<b< 101:
+            elif 204 < r < 255 and 135 < g < 215 and 21 < b < 101:
                 a = 0
-            elif 180<r<255 and 42<g<122 and 64<b<144:
+            elif 180 < r < 255 and 42 < g < 122 and 64 < b < 144:
                 a = 0
-            elif 132<r<212 and 22<g<102 and 190<b<255:
+            elif 132 < r < 212 and 22 < g < 102 and 190 < b < 255:
                 a = 0
             else:
                 a = 255
 
-            img2.putpixel((x,y),(a,a,a,255))
-    
+            img2.putpixel((x, y), (a, a, a, 255))
+
     builder = pyocr.builders.TextBuilder(tesseract_layout=6)
     builder.tesseract_configs.append("-c")
-    builder.tesseract_configs.append("tessedit_char_whitelist=\"EASYNORMLHDXPT\"")
+    builder.tesseract_configs.append('tessedit_char_whitelist="EASYNORMLHDXPT"')
     data = tool.image_to_string(img2, lang="eng", builder=builder)
-    
+
     return check(data, musicId)
--- app/title.py
+++ app/title.py
@@ -15,8 +15,9 @@ tool = tools[0]
 
 border = 215
 
-def check (target):
-    with open('./json/music.json', encoding="utf-8") as f1:
+
+def check(target):
+    with open("./json/music.json", encoding="utf-8") as f1:
         music = json.load(f1)
 
         data = {}
@@ -25,36 +26,39 @@ def check (target):
             result = difflib.SequenceMatcher(None, target, j["title"]).ratio()
 
             if result > 0.6:
-                data = ({"title":j["title"], "credibility":result, "music": j})
+                data = {"title": j["title"], "credibility": result, "music": j}
 
         data["ocr"] = target
         return data
 
+
 def title(url):
     img = openImg(url)
-    
-    rgb_img = img.convert('RGB')
+
+    rgb_img = img.convert("RGB")
     size = rgb_img.size
-    
-    crop_img = rgb_img.crop([0, 0, size[0]/2, size[1]/7])
+
+    crop_img = rgb_img.crop([0, 0, size[0] / 2, size[1] / 7])
     crop_size = crop_img.size
-    
-    img2 = Image.new('RGBA',crop_size)
-    
+
+    img2 = Image.new("RGBA", crop_size)
+
     for x in range(crop_size[0]):
         for y in range(crop_size[1]):
-            r,g,b = crop_img.getpixel((x,y))
+            r, g, b = crop_img.getpixel((x, y))
 
             if r >= border and g >= border and b >= border:
                 a = 255
             else:
                 a = 0
 
-            img2.putpixel((x,y),(a,a,a,255))
-    
-    crop_range = img2.convert('RGB').getbbox()
-    img3 = img2.crop([crop_range[0],crop_range[1]-5,crop_range[2],(crop_range[3]) / 2])
-    
+            img2.putpixel((x, y), (a, a, a, 255))
+
+    crop_range = img2.convert("RGB").getbbox()
+    img3 = img2.crop(
+        [crop_range[0], crop_range[1] - 5, crop_range[2], (crop_range[3]) / 2]
+    )
+
     builder11 = pyocr.builders.TextBuilder(tesseract_layout=11)
     data = tool.image_to_string(img3, lang="jpn", builder=builder11)
 
