a3efa7f2d80c6e1e596e870b779bd0d3804c7bf8
Cosmic-rare <ysolakey1116@gmail.com>
2023-02-23 10:55:08 +0900

    format by black

--- app/main.py
+++ app/main.py
@@ -4,33 +4,13 @@ from score import score
 from title import title
 from difficult import difficult
 
-app=FastAPI(  ) 
-
-
+app = FastAPI()
 
 
 @app.get("/")
 def upload_file(url):
-
-
-
-
-
-    scoreData               = score(url)
+    scoreData = score(url)
     titleData = title(url)
     difficultData = difficult(url, titleData["music"]["id"])
 
-    return {"score": scoreData, 
-
-
-
-
-
-
-
-            "title":       
-            titleData, 'difficult': difficultData
-
-
-
-        }    
+    return {"score": scoreData, "title": titleData, "difficult": difficultData}
