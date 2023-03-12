7330ed6d149e4c9d32872b197afcddf4491d5678
tani <ysolakey1116@gmail.com>
2023-03-13 06:13:11 +0900

    色の範囲の最適化＆リファクタリング1

--- final/templateMatch.py
+++ final/templateMatch.py
@@ -16,52 +16,42 @@ from matplotlib import pyplot as plt
 
 # search_contentとsearch_targetはRGBのNumpyArray
 def do_matching(search_content, search_target):
-  print(search_target.shape)
+  color_range = 25
+ 
   search_target = search_target[
     search_target.shape[0] // 2 : search_target.shape[0] // 8 * 7,
     0 : search_target.shape[1] // 4 * 3
   ]
-  print(search_target.shape)
-
-  print(search_content.shape)
 
   if search_target.shape[0] <= search_content.shape[0]:
     ratio = 1 - (search_content.shape[0] - search_target.shape[0]) / search_content.shape[0]
   else:
     ratio = 1
-
-  print(ratio)
-  print(search_content.shape)
-
   search_content = cv2.resize(search_content, None, None, ratio, ratio)
 
   search_target_ratio = get_ratio(search_content=search_content, search_target=search_target)['search_target']
 
   search_target = cv2.resize(search_target, None, None, search_target_ratio, search_target_ratio)
 
-  param = "TM_CCOEFF_NORMED"
-
-  result = cv2.matchTemplate(search_target, search_content, getattr(cv2, param))
+  result = cv2.matchTemplate(search_target, search_content, getattr(cv2, 'TM_CCOEFF_NORMED'))
   minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)
 
   tl = maxLoc
   br = (tl[0] + search_content.shape[1], tl[1] + search_content.shape[0])
 
   dst = search_target.copy()
-
   dst = dst[
     tl[1] + search_content.shape[0] // 10 * 3 - 5 : tl[1] + search_content.shape[0] // 10 * 3 + 5,
     tl[0] : dst.shape[1]
   ]
 
   np_dst = np.full((dst.shape[0], dst.shape[1]), 0)
-  color_range = 25
 
   for y in range(dst.shape[0]):
     for x in range(dst.shape[1]):
       r, g, b = dst[y][x]
 
-      if r==0 and g==255 and b==221:
+      if r<=70 and 215 <= g <= 255 and 205<=b<=236:
           a = 0
       elif 114-color_range < r < 144+color_range and 241-color_range < g < 241+color_range and 219-color_range < b < 219+color_range:
           a = 0
@@ -92,6 +82,6 @@ def do_matching(search_content, search_target):
   
 if __name__ == '__main__':
   search_content = cv2.cvtColor(cv2.imread('./final/img2.png'), cv2.COLOR_BGR2RGB)
-  search_target = cv2.cvtColor(cv2.imread('./targets/normal.png'), cv2.COLOR_BGR2RGB)
+  search_target = cv2.cvtColor(cv2.imread('./targets/target.jpg'), cv2.COLOR_BGR2RGB)
 
   print(do_matching(search_content=search_content, search_target=search_target))
